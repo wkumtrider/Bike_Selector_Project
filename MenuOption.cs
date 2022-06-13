fb636826baabb19d69dc20eb8b5a126fba2deead
Jason McCarty <jmccart480@gmail.com>
2022-06-12 20:04:27 -0400

    Added MenuOption class

---
+++ MenuOption.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bike_Selector_Project
+{
+    public class MenuOption
+    {
+        public MenuOption(string itemText)
+        {
+            itemText = itemText.Trim();
+        }
+        public string ItemText { get; set; }
+    }
+}
