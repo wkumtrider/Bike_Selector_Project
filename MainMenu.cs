4f9391283493df4fd604e7d2c903f2534b1ba22d
Jason McCarty <jmccart480@gmail.com>
2022-06-12 20:10:10 -0400

    Added additional code

--- MainMenu.cs
+++ MainMenu.cs
@@ -12,17 +12,27 @@ namespace Bike_Selector_Project
         {
             return new List<MenuOption>
            {
-               new MenuOption ("XC"),
-               new MenuOption ("All Mountain"),
-               new MenuOption ("Enduro"),
-               new MenuOption ("Down Hill"),
+               new MenuOption ("Trek"),
+               new MenuOption ("Giant"),
+               new MenuOption ("Pivot"),
+               new MenuOption ("Ibis"),
+               new MenuOption ("Transition"),
+               new MenuOption ("Specialized"),
+               new MenuOption ("Kona"),
            };
         }
 
         public void DisplayMainMenu(List<MenuOption> options)
         {
             Console.Clear();
-            Console.WriteLine("Please choose the type of bike you are interet")
+            Console.WriteLine("Please choose a bike brand from the menu below to see availabe models for sale");
+            Console.WriteLine("To exit the database please type 'Exit' and press enter");
+
+            int menuOptionCount = 1;
+            foreach(var option in options)
+            {
+                Console.WriteLine($"{menuOptionCount++}.{option.ItemText}");
+            }
         }
     }
 }
