26821ac09087f6d93d9642e5fbdb62bf1e23234d
Jason McCarty <jmccart480@gmail.com>
2022-07-04 11:34:03 -0400

    Added ReturnModels Class to contain LINQ code

---
+++ ReturnModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bike_Selector_Project
+{
+    public class ReturnModels
+    {
+
+        
+    }
+}
