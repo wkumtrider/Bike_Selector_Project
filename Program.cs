1f1bcdad4830fc14a2e8a9b3e35bd9afa3352b5b
Jason McCarty <jmccart480@gmail.com>
2022-07-06 19:23:10 -0400

    Added LINQ code to Program.cs

--- Program.cs
+++ Program.cs
@@ -14,33 +14,62 @@ namespace Bike_Selector_Project
         
         public static int InitInventory()
         {
+            string JsonFile = @"./Bikes.json";
 
-            string jsonString = File.ReadAllText("Bikes.json");
-            var bikes = JsonSerializer.Deserialize<List<Bike>>(jsonString);            
+            string jsonString = File.ReadAllText(JsonFile);
+            List<Bike> bikes = JsonSerializer.Deserialize<List<Bike>>(jsonString);
 
             MainMenu menu = new MainMenu();
             var options = menu.BuildMainMenu();
             menu.DisplayMainMenu(options);
             var bikeSelected = menu.GetMenuSelection(options);
 
-            var bikeType = from type in bikes where type.BikeType == "XC" select type;
-            
-            foreach (var i in bikeType)
+            //Console.WriteLine($"You selected menu option {bikeSelected}");
+            //return bikeSelected;
+            do
             {
-                Console.WriteLine(i);
-            }
+                switch (bikeSelected)
+                {
+                    case 1:
+                        var trek = bikes.Where(b => b.BikeBrand == "Trek").Select(b => b.BikeModel).ToList();
+                        foreach (var b in trek)
+                            Console.WriteLine($"Trek has the model {b} model available.");
+                        break;
+                    case 2:
+                        var giant = bikes.Where(b => b.BikeBrand == "Giant").Select(b => b.BikeModel).ToList();
+                        foreach (var b in giant)
+                            Console.WriteLine($"Giant has the {b} model available.");
+                        break;
+                    case 3:
+                        var ibis = bikes.Where(b => b.BikeBrand == "Ibis").Select(b => b.BikeModel).ToList();
+                        foreach (var b in ibis)
+                            Console.WriteLine($"Ibis has the {b} model available.");
+                        break;
+                    case 4:
+                        var pivot = bikes.Where(b => b.BikeBrand == "Pivot").Select(b => b.BikeModel).ToList();
+                        foreach (var b in pivot)
+                            Console.WriteLine($"Pivot has the {b} model available.");
+                        break;
+                    case 5:
+                        var transition = bikes.Where(b => b.BikeBrand == "Transition").Select(b => b.BikeModel).ToList();
+                        foreach (var b in transition)
+                            Console.WriteLine($"Transition has the {b} model available.");
+                        break;
+                    case 6:
+                        var specialized = bikes.Where(b => b.BikeBrand == "Specialized").Select(b => b.BikeModel).ToList();
+                        foreach (var b in specialized)
+                            Console.WriteLine($"Specialized has the {b} model available.");
+                        break;
+                    case 7:
+                        var kona = bikes.Where(b => b.BikeBrand == "Kona").Select(b => b.BikeModel).ToList();
+                        foreach (var b in kona)
+                            Console.WriteLine($"Kona has the {b} model available.");
+                        break;
 
-            foreach (var bike in bikes)
-            {
-                Console.WriteLine(bike.BikeBrand);
-                Console.WriteLine(bike.BikeType);
-                Console.WriteLine(bike.BikeModel);
-                Console.WriteLine("===========================");
-            }
-            Console.WriteLine($"You selected menu option {bikeSelected}");
-            return bikeSelected;
+                }
 
-            
+                return 0;
+            }while (true);
 
         }
     }
